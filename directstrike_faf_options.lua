442bd2aeb98c74c32c6bb8f54fa352854355222e
P4r4lon <68599705+P4r4lon@users.noreply.github.com>
2021-06-16 16:44:52 +0300

    Update directstrike_faf_options.lua

--- directstrike_faf_options.lua
+++ directstrike_faf_options.lua
@@ -1,32 +1,32 @@
 options = {
     {
-		default = 1,
+	default = 1,
         label = "Start cashpoints",
         help = "how many cash will u have for the 1st wave",
         key = 'StartCash',
         pref = 'StartCash',
         values = {
-			{
+           {
               text = "100",
               help = "",
               key = 100,
            },
-            {
+           {
               text = "200",
               help = "",
               key = 200,
            },
-            {
+           {
               text = "1000",
               help = "",
               key = 1000,
            },
-            {
+           {
               text = "5000",
               help = "",
               key = 5000,
            },
-			{
+	   {
               text = "500000",
               help = "",
               key = 500000,
@@ -35,139 +35,135 @@ options = {
        },
     },
     {
-		default = 1,
+	default = 1,
         label = "Wave timer",
         help = "how long does it take for new units to appear",
         key = 'WaveTimer',
         pref = 'WaveTimer',
         values = {
-			{
+	   {
               text = "30",
               help = "",
               key = 30,
            },
-            {
+           {
               text = "60",
               help = "",
               key = 60,
            },
-            {
+           {
               text = "15",
               help = "",
               key = 15,
            },
-            {
+           {
               text = "120",
               help = "",
               key = 120,
            },
        },
     },
-	 {
-		default = 1,
+    {
+	default = 1,
         label = "Income",
         help = "how much cash per sec",
         key = 'Income',
         pref = 'Income',
         values = {
-			{
+	   {
               text = "1",
               help = "",
               key = 1,
            },
-            {
+           {
               text = "3",
               help = "",
               key = 3,
            },
-            {
+           {
               text = "10",
               help = "",
               key = 10,
            },
-            {
+           {
               text = "50",
               help = "",
               key = 50,
            },
        },
     },
-	{
-		default = 2,
+    {
+	default = 2,
         label = "Wipe",
         help = "how many waves did the unit buy for?",
         key = 'Wipe',
         pref = 'Wipe',
         values = {
-			{
+  	   {
               text = "10",
               help = "",
               key = 10,
            },
-            {
+           {
               text = "5",
               help = "",
               key = 5,
            },
-            {
+           {
               text = "1",
               help = "",
               key = 1,
            },
-            {
+           {
               text = "100",
               help = "",
               key = 100,
            },
        },
     },
-	
-	
-	
-	{
-		default = 1,
+		
+    {
+	default = 1,
         label = "ExpWipe",
         help = "how many waves did the exp buy for?",
         key = 'ExpWipe',
         pref = 'ExpWipe',
         values = {
-			{
+	   {
               text = "1",
               help = "",
               key = 1,
            },
-            {
+           {
               text = "2",
               help = "",
               key = 2,
            },
-            {
+           {
               text = "3",
               help = "",
               key = 3,
            },
-            {
+           {
               text = "5",
               help = "",
               key = 5,
            },
        },
     },
-	
-	
-		{
-		default = 2,
+    {
+	default = 2,
         label = "Language",
         help = "Select ur language",
         key = 'Language',
         pref = 'Language',
         values = {
-			{
+	   {
               text = "RU",
               help = "",
               key = 'RU',
            },
-            {
+           {
               text = "EN",
               help = "",
               key = 'EN',
@@ -175,19 +171,19 @@ options = {
        },
     },
 	
-	{
-		default = 1,
+    {
+	default = 1,
         label = "Manual",
         help = "Do u need start manual?",
         key = 'Manual',
         pref = 'Manual',
         values = {
-			{
+	   {
               text = "Yes",
               help = "",
               key = 'true',
            },
-            {
+           {
               text = "No",
               help = "",
               key = 'false',
@@ -195,38 +191,23 @@ options = {
        },
     },
 	
-		{
-		default = 2,
+    {
+	default = 2,
         label = "HeroesOn",
         help = "Do u need imba bois here?",
         key = 'HeroesOn',
         pref = 'HeroesOn',
         values = {
-			{
+	   {
               text = "Yes",
               help = "",
               key = 1,
            },
-            {
+           {
               text = "No",
               help = "",
               key = 0,
            },
        },
-    },
-	
-	
-	
-	
-	
+    },	
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
